129cff1c0ef342928022f7cc6a24e111818e7670
Alexander Chernyak <sasha.chernya.04@mail.ru>
2025-04-09 17:27:38 +0300

    Add logout

--- RecommendationSystemAPI/users/urls.py
+++ RecommendationSystemAPI/users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenBlacklistView
 
 from .views import RegisterView, UserDetailView, ChangePasswordView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='logout'),
     path('me/', UserDetailView.as_view(), name='user_detail'),
     path('change-password/', ChangePasswordView.as_view(), name='change_password'),
 ]
