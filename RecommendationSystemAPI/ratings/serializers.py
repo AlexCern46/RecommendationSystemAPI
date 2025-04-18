9a7313f24af9b1dd2710ac768d453aa6092102eb
Alexander Chernyak <sasha.chernya.04@mail.ru>
2025-04-18 14:54:49 +0300

    Fix owner permission

--- RecommendationSystemAPI/ratings/serializers.py
+++ RecommendationSystemAPI/ratings/serializers.py
@@ -7,9 +7,21 @@ class MovieRatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieRating
         fields = '__all__'
+        read_only_fields = ['user', 'created_at']
+
+    def validate_rating(self, value):
+        if value < 1 or value > 10:
+            raise serializers.ValidationError("Invalid value")
+        return value
 
 
 class BookRatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookRating
         fields = '__all__'
+        read_only_fields = ['user', 'created_at']
+
+    def validate_rating(self, value):
+        if value < 1 or value > 10:
+            raise serializers.ValidationError("Invalid value")
+        return value
--- RecommendationSystemAPI/ratings/views.py
+++ RecommendationSystemAPI/ratings/views.py
@@ -1,5 +1,6 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, serializers
 
+from .models import MovieRating, BookRating
 from .serializers import MovieRatingSerializer, BookRatingSerializer
 from users.pemissions import IsOwnerOrReadOnly
 
@@ -8,17 +9,41 @@ class MovieRatingCreateView(generics.CreateAPIView):
     serializer_class = MovieRatingSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
+    def perform_create(self, serializer):
+        movie = serializer.validated_data['movie']
+        if MovieRating.objects.filter(user=self.request.user, movie=movie).exists():
+            raise serializers.ValidationError("Only one rev")
+        serializer.save(user=self.request.user)
+
 
 class MovieRatingView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MovieRatingSerializer
     permission_class = (IsOwnerOrReadOnly,)
+    queryset = MovieRating.objects.select_related('user', 'movie')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            user=self.request.user
+        )
 
 
 class BookRatingCreateView(generics.CreateAPIView):
     serializer_class = BookRatingSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
+    def perform_create(self, serializer):
+        movie = serializer.validated_data['book']
+        if BookRating.objects.filter(user=self.request.user, movie=movie).exists():
+            raise serializers.ValidationError("Only one rev")
+        serializer.save(user=self.request.user)
+
 
 class BookRatingView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BookRatingSerializer
     permission_class = (IsOwnerOrReadOnly,)
+    queryset = BookRating.objects.select_related('user', 'book')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(
+            user=self.request.user
+        )
